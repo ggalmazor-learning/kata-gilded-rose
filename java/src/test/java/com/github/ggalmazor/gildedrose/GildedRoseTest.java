ac554dda3c5e5079058f6866833b26b4189f46bc
Guillermo Gutiérrez <ggalmazor@gmail.com>
2019-06-11 10:06:07 +0200

    Java8 base exercise with Gradle

---
+++ java/src/test/java/com/github/ggalmazor/gildedrose/GildedRoseTest.java
@@ -0,0 +1,13 @@
+package com.github.ggalmazor.gildedrose;
+
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+public class GildedRoseTest {
+  @Test
+  public void name() {
+    assertThat(true, Matchers.is(false));
+  }
+}
